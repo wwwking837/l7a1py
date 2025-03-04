325b75c8a24527197b9475cf5d942149eb88f7a8
wwwking837 <muhammadzaroonadnan@gmail.com>
2025-03-04 18:21:37 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+my_tuple = ()
+print(my_tuple)
+
+my_tuple = (1,2,3)
+print(my_tuple)
+
+my_tuple = ("Hello","World")
+print(my_tuple)
+
+my_tuple = (1,2,3,"Hello","World")
+print(my_tuple)
+
+my_tuple = (4.6, 7.8, 9,3)
+print(my_tuple)
+
+my_tuple = (3, 4.6,"Dog")
+print(my_tuple)
+
+#nested tuple
+
+my_tuple = ("mouse", [8,4,6], (1,2,3))
+print(my_tuple)
+
+my_tuple = ('s', 't', 'r', 'i', 'n', 'g')
+print(my_tuple[0])
+print(my_tuple[5])
+
+num_tuple = [10, 20, 30, 40]
+print("max value:", max(num_tuple))
+print("min value:", min(num_tuple))
+
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+contcatenated_tuple = tuple1 + tuple2
+print("concatenated tuple:", contcatenated_tuple)
+
+num_tuple = (10, 20, 30, 40)
+sum_of_the_elements = sum(num_tuple)
+print("sum of elements=", sum_of_the_elements)
+
+
